23a5a98f0904a2f1ab3f99f8fadb35c70cfa1bfe
Jenniffer Roque <jennifferg1008@gmail.com>
2022-03-05 15:52:22 -0600

    “Models”

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -6,7 +6,7 @@ namespace _2020RC605_2020UL601.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Producto { get; set; }
+        public string Producto { get; set; } //ew
         public double Precio { get; set; }
     }
 }
